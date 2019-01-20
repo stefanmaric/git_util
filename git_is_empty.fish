89915495e92972559207913ca220c4c1e65ca22e
Stefan Maric <me@stefanmaric.com>
2019-01-20 18:30:39 +0100

    refactor: remove unncessary if+return statements

--- git_is_empty.fish
+++ git_is_empty.fish
@@ -1,8 +1,3 @@
 function git_is_empty -d "Test if a repository is empty"
-    if git_is_repo
-        and test -z (command git rev-list -n 1 --all 2>/dev/null)
-        return 0
-    else
-        return 1
-    end
+    git_is_repo; and test -z (command git rev-list -n 1 --all 2>/dev/null)
 end
